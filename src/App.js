7044c35db55cc523f3356dcf822ab899f171208b
Gabe Gonzales <gq.gonzales1@gmail.com>
2021-10-26 13:06:01 -0500

    Redux tutorial

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./actions";
 
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const authentication = useSelector((state) => state.isAuthenticated);
+  const dispatch = useDispatch();
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <div>Hello, world!</div>
+        <h2>Counter: {counter}</h2>
+        <button
+          className="btn btn-increase"
+          onClick={() => dispatch(increment(5))}
         >
-          Learn React
-        </a>
+          +5
+        </button>
+        <button
+          className="btn btn-increase"
+          onClick={() => dispatch(increment(1))}
+        >
+          +1
+        </button>
+        <button
+          className="btn btn-decrease"
+          onClick={() => dispatch(decrement())}
+        >
+          -1
+        </button>
+        <br></br>
+        <br></br>
+        <br></br>
+        {authentication ? (
+          <>
+            {" "}
+            <h4>Sensistive information!!!!</h4>
+          </>
+        ) : null}
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,14 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createStore } from "redux";
+import rootReducer from "./reducers";
+import { Provider } from "react-redux";
+
+const store = createStore(rootReducer);
+
+// Remember, Redux makes me SADR:
+
+// STORE is globalized state.
+// Import {createStore} from ‘redux’
+
+// ACTION describes what you want to do. Function that returns an object
+
+// const increment = () => {
+//   return {
+//     type: "INCREMENT",
+//   };
+// };
+
+// const decrement = () => {
+//   return {
+//     type: "DECREMENT",
+//   };
+// };
+
+// REDUCER checks which action you performed and modifies Store accordingly.
+// const counter = (state = 0, action) => {
+// eslint-disable-next-line default-case
+//   switch (action.type) {
+//     case "INCREMENT":
+//       return state + 1;
+//     case "DECREMENT":
+//       return state - 1;
+//   }
+// };
+
+// let store = createStore(counter);
+
+// store.subscribe(() => console.log(store.getState()));
+
+// DISPATCH executes an action to the reducer.
+// store.dispatch(increment());
+// store.dispatch(decrement());
+// store.dispatch(increment());
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
